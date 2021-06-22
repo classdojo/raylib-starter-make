370f8562ddd88322836ee923ed604568b3e8c08d
Liam Don <liamdon@gmail.com>
2021-06-22 14:53:12 -0700

    Update to latest API

--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,7 @@ int main() {
         {
             window.ClearBackground(raylib::Color(RAYWHITE));
 
-            camera.BeginMode3D();
+            camera.BeginMode();
             {
                 DrawPlane(Vector3{ 0.0f, 0.0f, 0.0f }, Vector2{ 32.0f, 32.0f }, LIGHTGRAY);  // Draw ground
                 DrawCube(Vector3{ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
@@ -73,7 +73,7 @@ int main() {
                     positions[i].DrawCubeWires(2.0f, heights[i], 2.0f, MAROON);
                 }
             }
-            camera.EndMode3D();
+            camera.EndMode();
 
             DrawRectangle(10, 10, 220, 70, raylib::Color(SKYBLUE).Fade(0.5f));
             DrawRectangleLines(10, 10, 220, 70, BLUE);
